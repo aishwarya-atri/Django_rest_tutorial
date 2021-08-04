ea80a0684c23dfcb26d0c3562d54f58b71227acf
Aishwarya Mustoori <mustoori@usc.edu>
2021-08-04 14:39:43 -0700

    feat(Django Rest Tutorial) :   Authentication and permissions
    Authentication and permissions
    
    [ ICTRAINING-802 ]

--- snippets/views.py
+++ snippets/views.py
@@ -12,16 +12,34 @@ from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 from rest_framework import mixins
 from rest_framework import generics
+from snippets.serializers import UserSerializer
+from django.contrib.auth.models import User
+from rest_framework import permissions
+
+from snippets.permissions import IsOwnerOrReadOnly
 
 class SnippetList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly]
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly]
     queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer
 
+    serializer_class = SnippetSerializer
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 # class SnippetList(mixins.ListModelMixin,
 #                   mixins.CreateModelMixin,
 #                   generics.GenericAPIView):
